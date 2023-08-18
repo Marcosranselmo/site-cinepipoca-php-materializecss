fbf2eb6b6adf6f8a159f0bf4ee9c9265a793641b
marcos <marcos.anselmo@hotmial.com>
2023-08-18 09:39:20 -0300

    Fist Commit

---
+++ galeria.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    
+<title>Cine Pipoca</title>
+</head>
+<body>
+    <nav class="nav-extended blue lighten-3">
+        <div class="nav-wrapper">
+            <ul id="nav-mobile" class="right">
+                <li><a href="galeria.php">Galeria</a></li>
+                <li><a href="cadastrar.php">Cadastrar</a></li>
+            </ul>
+        </div>
+        <div class="nav-header center">
+            <h1>CINEPIPOCA</h1>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent  #2196f3 blue">
+                <li class="tab"><a class="active" href="#test1">Todos</a></li>
+                <li class="tab"><a href="#test2">Assistidos</a></li>
+                <li class="tab disabled"><a href="#test3">Favoritos</a></li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
