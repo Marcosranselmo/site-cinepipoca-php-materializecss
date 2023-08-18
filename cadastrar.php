ac2f7b16cff22d3fcda76af062b1fbcd653d6a81
marcos <marcos.anselmo@hotmial.com>
2023-08-18 19:08:00 -0300

    feat

--- cadastrar.php
+++ cadastrar.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-    <title>Cadastrar</title>
-</head>
+<?php include "cabecalho.php" ?>
 
 <body>
     <nav class="nav-extended blue lighten-3">
--- galeria.php
+++ galeria.php
@@ -1,20 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include "cabecalho.php" ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php
 
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+$filme1 = [
+    "titulo" => "vingadores",
+    "nota" => "9,7",
+    "sinopse" => "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.",
+    "poster" => "https://www.themoviedb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
+];
 
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+$filme2 = [
+    "titulo" => "Ad Astra",
+    "nota" => "8,7",
+    "sinopse" => "Roy McBride é um engenheiro espacial, portador de um leve grau de autismo, que decide empreender a maior jornada de sua vida: viajar para o espaço, cruzar a galáxia e tentar descobrir o que aconteceu com seu pai, um astronauta que se perdeu há vinte anos atrás no caminho para Netuno.",
+    "poster" => "https://www.themoviedb.org/t/p/w300/wigZBAmNrIhxp2FNGOROUAeHvdh.jpg"
+];
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+$filme3 = [
+    "titulo" => "A Grande Comédia",
+    "nota" => "7,9",
+    "sinopse" => "Kevin Bacon estrela esta comédia no papel de Nick Chapman, um jovem e promissor ator que acaba de chegar a Hollywood. Para alcançar o sucesso, ele tentará agradar a todos que puderem lhe ajudar a realizar seus sonhos de luxo e da fama. Mas o sabor do fracasso lhe ensinará uma dura lição.",
+    "poster" => "https://www.themoviedb.org/t/p/w300/70273ysWBavPybak9teRNEDqDKz.jpg"
+];
 
-    <title>Cine Pipoca</title>
-</head>
+$filme4 = [
+    "titulo" => "As Aventuras de TIMTIM",
+    "nota" => "8,5",
+    "sinopse" => "As Aventuras de Tintim é uma série franco-belga criada por Hergé adaptando para TV os livros da série. Estreou em 1991 e teve 39 episódios divididos em três temporadas.",
+    "poster" => "https://www.themoviedb.org/t/p/w300/umlovDiFIbnR0mkFsDzmiHoxVOH.jpg"
+];
+
+$filmes = [$filme1, $filme2, $filme3, $filme4];
+
+?>
 
 <body>
     <nav class="nav-extended blue lighten-3">
@@ -37,38 +55,25 @@
     </nav>
 
     <div class="div row">
-        <div class="col s3">
-            <div class="card hoverable">
-                <div class="card-image">
-                    <img src="https://www.themoviedb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg">
-                    <a class="btn-floating halfway-fab waves-effect waves-light red">
-                        <i class="material-icons">favorite_border</i></a>
+        <?php foreach($filmes as $filme) : ?>
+
+                <div class="col s3">
+                    <div class="card hoverable">
+                        <div class="card-image">
+                            <img src="<?= $filme["poster"] ?>">
+                            <a class="btn-floating halfway-fab waves-effect waves-light red">
+                                <i class="material-icons">favorite_border</i></a>
+                        </div>
+                        <div class="card-content">
+                            <p class="valign-wrapper">
+                                <i class="material-icons amber-text">star</i> <?= $filme["nota"] ?>
+                            </p>
+                            <span class="card-title"><?= $filme["titulo"] ?></span>
+                            <p><?= $filme1["sinopse"] ?></p>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-content">
-                    <p class="valign-wrapper">
-                        <i class="material-icons amber-text">star</i> 9,7
-                    </p>
-                    <span class="card-title">Vingadores</span>
-                    <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
-                </div>
-            </div>
-        </div>
-        <div class="col s3">
-            <div class="card hoverable">
-                <div class="card-image">
-                    <img src="https://www.themoviedb.org/t/p/w300/wigZBAmNrIhxp2FNGOROUAeHvdh.jpg">
-                    <a class="btn-floating halfway-fab waves-effect waves-light red">
-                        <i class="material-icons">favorite_border</i></a>
-                </div>
-                <div class="card-content">
-                    <p class="valign-wrapper">
-                        <i class="material-icons amber-text">star</i> 9,7
-                    </p>
-                    <span class="card-title">Ad Astra</span>
-                    <p>Roy McBride é um engenheiro espacial, portador de um leve grau de autismo, que decide empreender a maior jornada de sua vida: viajar para o espaço, cruzar a galáxia e tentar descobrir o que aconteceu com seu pai, um astronauta que se perdeu há vinte anos atrás no caminho para Netuno.</p>
-                </div>
-            </div>
-        </div>
+        <?php endforeach ?>
     </div>
 
 </body>
