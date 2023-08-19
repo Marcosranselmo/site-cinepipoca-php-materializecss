d11cdfe30aaf78bec020cbba4ed0aaedd86dd809
marcos <marcos.anselmo@hotmial.com>
2023-08-19 20:29:26 -0300

    feat

--- bd_up.php
+++ bd_up.php
@@ -1,5 +1,44 @@
 <?php
+// CONEXÃO COM O BANCO DE DADOS CINEPIPOCA
+$host = "localhost";
+$user = "admin";
+$pass = "admin";
+$base = "db_cinepipoca";
 
-$bd = new SQLite3("filmes.db");
+// FAzENDO A CONEXÃO COM O BANCO DE DADOS
+$con_cinepipoca = mysqli_connect($host, $user, $pass);
+$banco = mysqli_select_db($con_cinepipoca, $base);
+
+// MENSAGEM DE FALHA DE CONEXÃO
+if(mysqli_connect_errno()) {
+    die("Falha na conexão com o bando de dados: ".
+        mysqli_connect_error() . " ( " . mysqli_connect_errno() . " )"
+    );
+}
+	
+$sql = "CREATE TABLE filmes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    titulo VARCHAR(200) NOT NULL,
+    poster VARCHAR (200),
+    sinopse TEXT,
+    nota DECIMAL(2,1)
+    )
+";
+
+if ($bd->exec($sql))
+    echo "\ntabela filmes criada\n";
+else
+    echo "\nerro ao criar tabela filmes\n"; 
+
+$sql = "INSERT INTO filmes (id, titulo, poster, sinopse, nota) VALUES (
+        0,
+    )";
+
+$filme1 = [
+    "titulo" => "vingadores",
+    "nota" => "9,7",
+    "sinopse" => "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.",
+    "poster" => "https://www.themoviedb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
+];
 
 ?>
--- galeria.php
+++ galeria.php
@@ -54,26 +54,27 @@ $filmes = [$filme1, $filme2, $filme3, $filme4];
         </div>
     </nav>
 
-    <div class="div row">
-        <?php foreach($filmes as $filme) : ?>
-
-                <div class="col s3">
-                    <div class="card hoverable">
-                        <div class="card-image">
-                            <img src="<?= $filme["poster"] ?>">
-                            <a class="btn-floating halfway-fab waves-effect waves-light red">
-                                <i class="material-icons">favorite_border</i></a>
-                        </div>
-                        <div class="card-content">
-                            <p class="valign-wrapper">
-                                <i class="material-icons amber-text">star</i> <?= $filme["nota"] ?>
-                            </p>
-                            <span class="card-title"><?= $filme["titulo"] ?></span>
-                            <p><?= $filme1["sinopse"] ?></p>
+    <div class="container">
+        <div class="div row">
+            <?php foreach($filmes as $filme) : ?>
+                    <div class="col s3">
+                        <div class="card hoverable">
+                            <div class="card-image">
+                                <img src="<?= $filme["poster"] ?>">
+                                <a class="btn-floating halfway-fab waves-effect waves-light red">
+                                    <i class="material-icons">favorite_border</i></a>
+                            </div>
+                            <div class="card-content">
+                                <p class="valign-wrapper">
+                                    <i class="material-icons amber-text">star</i> <?= $filme["nota"] ?>
+                                </p>
+                                <span class="card-title"><?= $filme["titulo"] ?></span>
+                                <p><?= $filme1["sinopse"] ?></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-        <?php endforeach ?>
+            <?php endforeach ?>
+        </div>
     </div>
 
 </body>
