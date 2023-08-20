b7bc34032c2d38ff5d7742be7acb49ea166244a6
marcos <marcos.anselmo@hotmial.com>
2023-08-20 16:42:24 -0300

    feat

--- bd_up.php
+++ bd_up.php
@@ -1,44 +1,31 @@
 <?php
-// CONEXÃO COM O BANCO DE DADOS CINEPIPOCA
-$host = "localhost";
-$user = "admin";
-$pass = "admin";
-$base = "db_cinepipoca";
 
-// FAzENDO A CONEXÃO COM O BANCO DE DADOS
-$con_cinepipoca = mysqli_connect($host, $user, $pass);
-$banco = mysqli_select_db($con_cinepipoca, $base);
+$bd = new SQLite3("filmes.db");
 
-// MENSAGEM DE FALHA DE CONEXÃO
-if(mysqli_connect_errno()) {
-    die("Falha na conexão com o bando de dados: ".
-        mysqli_connect_error() . " ( " . mysqli_connect_errno() . " )"
-    );
-}
-	
-$sql = "CREATE TABLE filmes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    titulo VARCHAR(200) NOT NULL,
-    poster VARCHAR (200),
-    sinopse TEXT,
-    nota DECIMAL(2,1)
-    )
-";
+$sql = "DROP TABLE IF EXISTS filmes";
 
 if ($bd->exec($sql))
-    echo "\ntabela filmes criada\n";
+    echo "\ntabela filmes apagada\n";
+
+$sql = "CREATE TABLE filmes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        titulo VARCHAR(200) NOT NULL,
+        poster VARCHAR (200),
+        sinopse TEXT,
+        nota DECIMAL(2,1)
+    )   
+";
+
+$sql = "INSERT INTO filmes (id, titulo, poster, sinopse, nota) VAlUES (
+    0,
+    'Vingadores',
+    'https://www.themoviedb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg',
+    'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.'
+    9.9
+)"; 
+
+if ($bd->exec($sql))
+    echo "\ntabela filmes inseridos com sucesso\n";
 else
-    echo "\nerro ao criar tabela filmes\n"; 
-
-$sql = "INSERT INTO filmes (id, titulo, poster, sinopse, nota) VALUES (
-        0,
-    )";
-
-$filme1 = [
-    "titulo" => "vingadores",
-    "nota" => "9,7",
-    "sinopse" => "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.",
-    "poster" => "https://www.themoviedb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
-];
-
+    echo "\nerro ao inserir filmes filmes\n";
 ?>
--- galeria.php
+++ galeria.php
@@ -2,33 +2,33 @@
 
 <?php
 
-$filme1 = [
-    "titulo" => "vingadores",
-    "nota" => "9,7",
-    "sinopse" => "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.",
-    "poster" => "https://www.themoviedb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
-];
+// $filme1 = [
+//     "titulo" => "vingadores",
+//     "nota" => "9,7",
+//     "sinopse" => "I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.",
+//     "poster" => "https://www.themoviedb.org/t/p/w300/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg"
+// ];
 
-$filme2 = [
-    "titulo" => "Ad Astra",
-    "nota" => "8,7",
-    "sinopse" => "Roy McBride é um engenheiro espacial, portador de um leve grau de autismo, que decide empreender a maior jornada de sua vida: viajar para o espaço, cruzar a galáxia e tentar descobrir o que aconteceu com seu pai, um astronauta que se perdeu há vinte anos atrás no caminho para Netuno.",
-    "poster" => "https://www.themoviedb.org/t/p/w300/wigZBAmNrIhxp2FNGOROUAeHvdh.jpg"
-];
+// $filme2 = [
+//     "titulo" => "Ad Astra",
+//     "nota" => "8,7",
+//     "sinopse" => "Roy McBride é um engenheiro espacial, portador de um leve grau de autismo, que decide empreender a maior jornada de sua vida: viajar para o espaço, cruzar a galáxia e tentar descobrir o que aconteceu com seu pai, um astronauta que se perdeu há vinte anos atrás no caminho para Netuno.",
+//     "poster" => "https://www.themoviedb.org/t/p/w300/wigZBAmNrIhxp2FNGOROUAeHvdh.jpg"
+// ];
 
-$filme3 = [
-    "titulo" => "A Grande Comédia",
-    "nota" => "7,9",
-    "sinopse" => "Kevin Bacon estrela esta comédia no papel de Nick Chapman, um jovem e promissor ator que acaba de chegar a Hollywood. Para alcançar o sucesso, ele tentará agradar a todos que puderem lhe ajudar a realizar seus sonhos de luxo e da fama. Mas o sabor do fracasso lhe ensinará uma dura lição.",
-    "poster" => "https://www.themoviedb.org/t/p/w300/70273ysWBavPybak9teRNEDqDKz.jpg"
-];
+// $filme3 = [
+//     "titulo" => "A Grande Comédia",
+//     "nota" => "7,9",
+//     "sinopse" => "Kevin Bacon estrela esta comédia no papel de Nick Chapman, um jovem e promissor ator que acaba de chegar a Hollywood. Para alcançar o sucesso, ele tentará agradar a todos que puderem lhe ajudar a realizar seus sonhos de luxo e da fama. Mas o sabor do fracasso lhe ensinará uma dura lição.",
+//     "poster" => "https://www.themoviedb.org/t/p/w300/70273ysWBavPybak9teRNEDqDKz.jpg"
+// ];
 
-$filme4 = [
-    "titulo" => "As Aventuras de TIMTIM",
-    "nota" => "8,5",
-    "sinopse" => "As Aventuras de Tintim é uma série franco-belga criada por Hergé adaptando para TV os livros da série. Estreou em 1991 e teve 39 episódios divididos em três temporadas.",
-    "poster" => "https://www.themoviedb.org/t/p/w300/umlovDiFIbnR0mkFsDzmiHoxVOH.jpg"
-];
+// $filme4 = [
+//     "titulo" => "As Aventuras de TIMTIM",
+//     "nota" => "8,5",
+//     "sinopse" => "As Aventuras de Tintim é uma série franco-belga criada por Hergé adaptando para TV os livros da série. Estreou em 1991 e teve 39 episódios divididos em três temporadas.",
+//     "poster" => "https://www.themoviedb.org/t/p/w300/umlovDiFIbnR0mkFsDzmiHoxVOH.jpg"
+// ];
 
 $filmes = [$filme1, $filme2, $filme3, $filme4];
 
