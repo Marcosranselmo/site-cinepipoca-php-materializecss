1e52f433513f5f998a5c56cbbcf4074231de135b
marcos <marcos.anselmo@hotmial.com>
2023-08-23 22:27:52 -0300

    feat

--- view/cabecalho.php
+++ view/cabecalho.php
@@ -7,8 +7,8 @@
 
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+   
     <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
